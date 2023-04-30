e480f7f6443d9fe5a74af89d1d1cd96c338d8ba3
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-04-30 22:19:43 +0200

    ExploreCard component

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -29,7 +29,7 @@ const Explore = () => {
         <TitleText title={<>Choose the world you want <br className="hidden md:block" /> to explore</>}
           textStyles="text-center"
         />
-        <div className="mt-[50px] flex lg:flew-row flex-col min-h-[70vh] gap-5">
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
             <ExploreCard
               key={world.id}
