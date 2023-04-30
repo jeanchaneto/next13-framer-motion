253c2a7f013d359daf2c1a7afe451fb56b9ebb76
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-04-30 16:39:32 +0200

    Implement About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,41 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components/CustomTexts';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-10" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About MetarversUI"
+        textStyles="text-center"
+      />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis soluta temporibus earum, incidunt aut placeat? Repellendus distinctio, iusto laudantium eaque omnis consequuntur ex voluptate magni assumenda corrupti dolor pariatur facere? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+
+    </motion.div>
+
   </section>
 );
 
